8666a08df423aeabe447a1d99bf49ee67d9a7bae
Anirbanjyoti <anirbanjyoti708@gmail.com>
2022-05-31 23:58:11 +0530

    params

--- src/Component/Friend/Friend.js
+++ src/Component/Friend/Friend.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Friends.css'
 
 const Friend = (props) => {
     const {id, name} = props.friend;
+
+    const navigate = useNavigate();
+    const showFriendDetails = () =>{
+            navigate('/Friend/'+id);
+    } 
+
     return (
         <div className='friend'>
                 <h1>{name}</h1>
-                <p><button> <Link to={'/Friend/'+id} >Go To Friends Details:</Link> </button></p>
+                <p><button> <Link to={'/Friend/'+id} >System one: Using Link to go to Details:</Link> </button></p>
+                <p><button onClick={showFriendDetails}> System Two: Using Onclick to go to Details</button></p>
+
+                
+
         </div>
     );
 };
--- src/Component/FriendDetails/FriendDetails.js
+++ src/Component/FriendDetails/FriendDetails.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const FriendDetails = () => {
+    const {friendID} = useParams();
     return (
         <div>
-            <h1>This your Friends details</h1>
+            <h1>This your Friends details: {friendID}</h1>
+
         </div>
     );
 };
